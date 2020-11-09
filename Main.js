ad882acab425f776d6d5f141f70071569fb969ab
Silva <silva@localhost.localdomain>
2020-11-09 19:45:10 +0100

    Revert "create Login"
    This reverts commit bbda6ac5f2faf8ac24b87098e75782b36304a663.

--- Main.js
+++
@@ -1,7 +0,0 @@
-var msg = 'Hello World';
-console.log(msg);
-
-const Person = require('./person');
-var person1 = new Person('John',30);
-console.log(person1);
-console.log(person1.getPersonStats());
